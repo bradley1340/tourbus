d9d64910d8b9988bc497c4d83fcbdc8d3218ba3d
bradley1340 <bradley1@gmail.com>
2026-03-18 14:32:36 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// Dynamic import to handle case-insensitive filename
-const App = React.lazy(() => import('./app.jsx'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <React.Suspense fallback={null}>
-      <App />
-    </React.Suspense>
+    <App />
   </React.StrictMode>
 )
