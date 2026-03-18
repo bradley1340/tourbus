1a1d94b30c2cd6f156e93819e98381c773a5dd15
bradley1340 <bradley1@gmail.com>
2026-03-18 14:30:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import App from './App.jsx'
 // Dynamic import to handle case-insensitive filename
 const App = React.lazy(() => import('./app.jsx'))
 
